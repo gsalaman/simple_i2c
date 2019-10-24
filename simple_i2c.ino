4d8700d0a0c31413ceae93a5ceee4d434552ec7d
Glenn Salaman <g.salaman@comcast.net>
2019-10-24 06:11:58 -0600

    debug prints...artemis version

--- simple_i2c.ino
+++ simple_i2c.ino
@@ -1,19 +1,50 @@
 
 #include <Wire.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Wire1.begin();
+bool wait=false;
+
+void wait_for_key( void )
+{
+  char c;
+
+  Serial.println("press any key to continue");
+  while (!Serial.available());
+  while (Serial.available()) c = Serial.read();  
 }
 
-void loop() {
+void debug_print_and_wait(char *str)
+{
+  if (wait)
+  {
+    Serial.println(str);
+    wait_for_key();
+  }
+}
+
+void setup() 
+{
+  Serial.println("Init");
+  Serial.begin(9600);
+  Wire1.begin();
   
-  // put your main code here, to run repeatedly:
+  debug_print_and_wait("init complete"); 
+}
+
+void loop() 
+{
+  debug_print_and_wait("begin transmission");
   Wire1.beginTransmission(0x20);
+  
+  debug_print_and_wait("write address"); 
   Wire1.write(0x03);
+  
+  debug_print_and_wait("end transmission");
   Wire1.endTransmission();
+  
+  debug_print_and_wait("request 1 byte");
   Wire1.requestFrom(0x20,1);
+  
+  debug_print_and_wait("read");
   if (Wire1.available()) Serial.println(Wire1.read());
   else Serial.println("Read fails");
 
