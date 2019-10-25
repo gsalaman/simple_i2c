d719d8957da38532230d5b208a14981046240b1e
Glenn Salaman <g.salaman@comcast.net>
2019-10-25 10:25:04 -0600

    Update simple_i2c.ino

--- simple_i2c.ino
+++ simple_i2c.ino
@@ -1,7 +1,7 @@
 
 #include <Wire.h>
 
-bool wait=true;
+bool wait=false;
 
 void wait_for_key( void )
 {
@@ -42,11 +42,25 @@ void loop()
   debug_print_and_wait("end transmission");
   Wire.endTransmission();
   
-  debug_print_and_wait("request 1 byte");
-  Wire.requestFrom(0x20,1);
+  debug_print_and_wait("request 4 bytes");
+  Wire.requestFrom(0x20,4);
   
   debug_print_and_wait("read");
-  if (Wire.available()) Serial.println(Wire.read());
+  
+  if (Wire.available() == 4) 
+  {
+    // The 4 bytes should be:
+    //    1) Horizontal MSB
+    //    2) Horizontal LSB
+    //    3) Vertical MSB
+    //    4) Vertical LSB
+    //  I only care about the MSBs, so the LSB reads will be ignored.
+    Serial.print("horiz: ");
+    Serial.println(Wire.read());
+    Wire.read();
+    Serial.print("vert:");
+    Serial.println(Wire.read());
+  }
   else Serial.println("Read fails");
 
   delay(1000);
